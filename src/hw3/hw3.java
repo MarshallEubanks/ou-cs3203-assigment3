62a461aa65380443085e8f3f93bc67bbd9b1e1e3
Marshall Eubanks <Marshall.K.Eubanks-1@ou.edu>
2023-09-13 22:56:23 -0500

    Step 3

--- src/hw3/hw3.java
+++ src/hw3/hw3.java
@@ -20,5 +20,46 @@ public class hw3 {
 		return produss;
 	}
 	
-
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter array size");
+		int size = scan.nextInt();
+		
+		int[] arr = new int[size];
+		
+		System.out.println("Input array elements");
+		
+		int element;
+		for (int i=0; i < arr.length;i++) {
+			element = scan.nextInt();
+			arr[i] = element;
+			
+		}
+		
+		System.out.println("Options:");
+		System.out.println("S - Sum of Array");
+		System.out.println("P - Product of Array");
+		System.out.println("X - Exit");
+		
+		String option = scan.next();
+		
+		while (!option.equals("X")) {
+			
+			if (option.equals("S")) {
+				int sum = sumArray(arr);
+				System.out.println(sum);
+			}
+			
+			else if (option.equals("P")) {
+				int product = multiplyArray(arr);
+				System.out.println(product);
+			}
+			
+			System.out.println("Enter next option");
+			option = scan.next();
+		}
+		
+		System.out.println("Options completed");
+		
+	}
 }
